01bcb87396badba4ecfd7cd27c2857db038fe346
Luiz Pires <lpiresjunior@gmail.com>
2023-04-16 03:45:52 -0300

    feat: test knex queries

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,22 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = knex('sqlite_schema').select('*')
+  // const transaction = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*')
 
-  return tables
+  // return transaction
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
 })
 
 app
